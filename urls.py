be0d10a9a2ab40931518b8aa91c306df5f4698e3
l4viniavieira <laviniavieira175@gmail.com>
2026-06-01 10:02:02 -0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from django.views.generic import TemplateView
 from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),]
+    path('', IndexView.as_view(), name='index'),
+    path('pessoas/', PessoaView.as_view(), name='pessoas'),
+    path('logs/', LogAtualizacaoView.as_view(), name='logs'),
+    path('comentarios/', ComentarioView.as_view(), name='comentarios'),
+    path('localizacoes/', LocalizacaoView.as_view(), name='localizacoes'),
+    path('instituicoes/', InstituicaoView.as_view(), name='instituicoes'),
+    path('areassaber/', AreaSaberView.as_view(), name='areassaber'),
+    path('eventos/', EventoGeopoliticoView.as_view(), name='eventosgeopoliticos'),
+    path('fontes/', FonteInformacaoView.as_view(), name='fontesinformacao'),
+    path('commodities/', CommodityView.as_view(), name='commodities'),
+    path('inflacao/', IndicadorInflacaoView.as_view(), name='indicadoresinflacao'),
+    path('migracoes/', FluxoMigratorioView.as_view(), name='fluxosmigratorios'),
+    path('rotascomerciais/', RotaComercialView.as_view(), name='rotascomerciais'),
+    path('impactos/', ImpactoEconomicoView.as_view(), name='impactos'),
+    path('nexoscausais/', NexoCausalView.as_view(), name='nexoscausais'),
+    path('relatorios/', RelatorioPaisView.as_view(), name='relatorios'),
+]
--- views.py
+++ views.py
@@ -1,10 +1,82 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.views import View
-from django.contrib import messages
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
-    def post(self, request):
-        pass
+
+class PessoaView(View):
+    def get(self, request, *args, **kwargs):
+        pessoas = Pessoa.objects.all()
+        return render(request, 'pessoa.html', {'pessoas': pessoas})
+
+class LogAtualizacaoView(View):
+    def get(self, request, *args, **kwargs):
+        logs = LogAtualizacao.objects.all()
+        return render(request, 'logatualizacao.html', {'logs': logs})
+
+class ComentarioView(View):
+    def get(self, request, *args, **kwargs):
+        comentarios = Comentario.objects.all()
+        return render(request, 'comentario.html', {'comentarios': comentarios})
+
+class LocalizacaoView(View):
+    def get(self, request, *args, **kwargs):
+        localizacoes = Localizacao.objects.all()
+        return render(request, 'localizacao.html', {'localizacoes': localizacoes})
+
+class InstituicaoView(View):
+    def get(self, request, *args, **kwargs):
+        instituicoes = Instituicao.objects.all()
+        return render(request, 'instituicao.html', {'instituicoes': instituicoes})
+
+class AreaSaberView(View):
+    def get(self, request, *args, **kwargs):
+        areassaber = AreaSaber.objects.all()
+        return render(request, 'areasaber.html', {'areas_saber': areassaber})
+
+class EventoGeopoliticoView(View):
+    def get(self, request, *args, **kwargs):
+        eventos = EventoGeopolitico.objects.all()
+        return render(request, 'eventogeopolitico.html', {'eventos': eventos})
+
+class FonteInformacaoView(View):
+    def get(self, request, *args, **kwargs):
+        fontes = FonteInformacao.objects.all()
+        return render(request, 'fonteinformacao.html', {'fontes': fontes})
+
+class CommodityView(View):
+    def get(self, request, *args, **kwargs):
+        commodities = Commodity.objects.all()
+        return render(request, 'commodity.html', {'commodities': commodities})
+
+class IndicadorInflacaoView(View):
+    def get(self, request, *args, **kwargs):
+        indicadores = IndicadorInflacao.objects.all()
+        return render(request, 'indicadorinflacao.html', {'indicadores': indicadores})
+
+class FluxoMigratorioView(View):
+    def get(self, request, *args, **kwargs):
+        fluxos = FluxoMigratorio.objects.all()
+        return render(request, 'fluxomigratorio.html', {'fluxos': fluxos})
+
+class RotaComercialView(View):
+    def get(self, request, *args, **kwargs):
+        rotascomerciais = RotaComercial.objects.all()
+        return render(request, 'rotacomercial.html', {'rotas': rotascomerciais})
+
+class ImpactoEconomicoView(View):
+    def get(self, request, *args, **kwargs):
+        impactos = ImpactoEconomico.objects.all()
+        return render(request, 'impactoeconomico.html', {'impactos': impactos})
+
+class NexoCausalView(View):
+    def get(self, request, *args, **kwargs):
+        nexos = NexoCausal.objects.all()
+        return render(request, 'nexocausal.html', {'nexoscausais': nexos})
+
+class RelatorioPaisView(View):
+    def get(self, request, *args, **kwargs):
+        relatorios = RelatorioPais.objects.all()
+        return render(request, 'relatoriopais.html', {'relatorios': relatorios})
